cb9c559acc3697bc220710f4bcf40dda49f7be23
Jarred Capellman <jcapellman@hotmail.com>
2018-02-18 21:50:55 -0600

    Added: Initial work on the Submission API Endpoint

---
+++ src/jcBENCH.web/Controllers/ResultSubmissionController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using jcBENCH.lib.Objects;
+using jcBENCH.web.DAL;
+using jcBENCH.web.DAL.Objects;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace jcBENCH.web.Controllers
+{
+    [Route("api/[controller]")]
+    public class ResultSubmissionController : Controller
+    {
+        private MainDbContext dbContext;
+
+        public ResultSubmissionController(MainDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpPost]
+        public async Task<bool> SubmitAsync([FromBody]ResultSubmissionItem submissionItem)
+        {
+            var resultItem = new Results
+            {
+                BenchmarkID = submissionItem.BenchmarkID,
+                CPUArchitecture = submissionItem.CPUArchitecture,
+                CPUFrequency = submissionItem.CPUFrequency,
+                CPUManufacturer = submissionItem.CPUManufacturer,
+                CPUName = submissionItem.CPUName,
+                OperatingSystem = submissionItem.OperatingSystem,
+                PlatformID = submissionItem.PlatformID,
+                Created = DateTime.Now
+            };
+
+            var result = await dbContext.Results.AddAsync(resultItem);
+
+            return await dbContext.SaveChangesAsync() > 0;
+        }
+    }
+}

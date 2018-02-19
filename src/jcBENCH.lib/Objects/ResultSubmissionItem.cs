17765a21a284500df73d2b01e038a138a7312ea0
Jarred Capellman <jcapellman@hotmail.com>
2018-02-18 21:55:50 -0600

    Added: ResultSubmissionItem class

---
+++ src/jcBENCH.lib/Objects/ResultSubmissionItem.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.Serialization;
+
+namespace jcBENCH.lib.Objects
+{
+    [DataContract]
+    public class ResultSubmissionItem
+    {
+        [DataMember]
+        public int BenchmarkResult { get; set; }
+
+        [DataMember]
+        public string BenchmarkID { get; set; }
+
+        [DataMember]
+        public string PlatformID { get; set; }
+
+        [DataMember]
+        public string CPUName { get; set; }
+
+        [DataMember]
+        public string CPUManufacturer { get; set; }
+
+        [DataMember]
+        public string CPUArchitecture { get; set; }
+
+        [DataMember]
+        public string CPUFrequency { get; set; }
+
+        [DataMember]
+        public string OperatingSystem { get; set; }
+    }
+}

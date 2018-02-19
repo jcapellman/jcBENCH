67b5f17fc736718cf6a570a9f0a9a63ea3ae6088
Jarred Capellman <jcapellman@hotmail.com>
2018-02-18 21:55:23 -0600

    Added: Initial Migration

---
+++ src/jcBENCH.web/Migrations/20180219030204_Initial.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace jcBENCH.web.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Benchmarks",
+                columns: table => new
+                {
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Benchmarks", x => x.Name);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Platforms",
+                columns: table => new
+                {
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Platforms", x => x.Name);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Results",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    BenchmarkID = table.Column<string>(nullable: false),
+                    CPUArchitecture = table.Column<string>(nullable: false),
+                    CPUFrequency = table.Column<string>(nullable: false),
+                    CPUManufacturer = table.Column<string>(nullable: false),
+                    CPUName = table.Column<string>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: false),
+                    OperatingSystem = table.Column<string>(nullable: false),
+                    PlatformID = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Results", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Results_Benchmarks_BenchmarkID",
+                        column: x => x.BenchmarkID,
+                        principalTable: "Benchmarks",
+                        principalColumn: "Name",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Results_Platforms_PlatformID",
+                        column: x => x.PlatformID,
+                        principalTable: "Platforms",
+                        principalColumn: "Name",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Results_BenchmarkID",
+                table: "Results",
+                column: "BenchmarkID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Results_PlatformID",
+                table: "Results",
+                column: "PlatformID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Results");
+
+            migrationBuilder.DropTable(
+                name: "Benchmarks");
+
+            migrationBuilder.DropTable(
+                name: "Platforms");
+        }
+    }
+}

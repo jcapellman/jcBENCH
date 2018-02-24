e93a2642fddb671cd8434704f0991cbaa675152c
Jarred Capellman <jcapellman@hotmail.com>
2018-02-24 11:20:05 -0600

    Added: Initial work on the macOS Library

---
+++ src/jcBENCH.lib.osx/MacOSDeviceInformation.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+
+using jcBENCH.lib.PlatformImplementations;
+
+namespace jcBENCH.lib.osx
+{
+    public class MacOSDeviceInformation : BaseDeviceInformation
+    {
+        public override OSPlatform Platform => OSPlatform.OSX;
+
+        public override string OperatingSystem => RuntimeEnvironment.GetSystemVersion();
+
+        public override (string manufacturer, string model, int numberCores, string frequency, string architecture) GetCpuInformation()
+        {
+            return (string.Empty, string.Empty, 0, string.Empty, string.Empty);
+        }
+    }
+}

ea198305dfed1ca676e2e9defcef3e3bcb976de2
Jarred Capellman <jcapellman@hotmail.com>
2018-02-26 22:05:57 -0600

    Added: Error Logging on submission with NLog

--- src/jcBENCH.web/Controllers/ResultSubmissionController.cs
+++ src/jcBENCH.web/Controllers/ResultSubmissionController.cs
@@ -6,12 +6,14 @@ using jcBENCH.web.DAL;
 using jcBENCH.web.DAL.Objects;
 
 using Microsoft.AspNetCore.Mvc;
+using NLog;
 
 namespace jcBENCH.web.Controllers
 {
     [Route("api/[controller]")]
     public class ResultSubmissionController : Controller
     {
+        private Logger Logger => NLog.LogManager.GetCurrentClassLogger();
         private MainDbContext dbContext;
 
         public ResultSubmissionController(MainDbContext dbContext)
@@ -22,22 +24,29 @@ namespace jcBENCH.web.Controllers
         [HttpPost]
         public async Task<bool> SubmitAsync([FromBody]ResultSubmissionItem submissionItem)
         {
-            var resultItem = new Results
+            try
             {
-                BenchmarkID = submissionItem.BenchmarkID,
-                BenchmarkResult = submissionItem.BenchmarkResult,
-                CPUArchitecture = submissionItem.CPUArchitecture,
-                CPUFrequency = submissionItem.CPUFrequency,
-                CPUManufacturer = submissionItem.CPUManufacturer,
-                CPUName = submissionItem.CPUName,
-                OperatingSystem = submissionItem.OperatingSystem,
-                PlatformID = submissionItem.PlatformID,
-                Created = DateTime.Now
-            };
+                var resultItem = new Results
+                {
+                    BenchmarkID = submissionItem.BenchmarkID,
+                    BenchmarkResult = submissionItem.BenchmarkResult,
+                    CPUArchitecture = submissionItem.CPUArchitecture,
+                    CPUFrequency = submissionItem.CPUFrequency,
+                    CPUManufacturer = submissionItem.CPUManufacturer,
+                    CPUName = submissionItem.CPUName,
+                    OperatingSystem = submissionItem.OperatingSystem,
+                    PlatformID = submissionItem.PlatformID,
+                    Created = DateTime.Now
+                };
 
-            var result = await dbContext.Results.AddAsync(resultItem);
+                var result = await dbContext.Results.AddAsync(resultItem);
 
-            return await dbContext.SaveChangesAsync() > 0;
+                return await dbContext.SaveChangesAsync() > 0;
+            } catch (Exception ex)
+            {
+                Logger.Error(ex, $"Failed to submit {submissionItem}");
+                return false;
+            }
         }
     }
 }

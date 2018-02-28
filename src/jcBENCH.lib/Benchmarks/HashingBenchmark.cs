7f244622f5be86cfa26a013765e22e8ea1143544
Jarred Capellman <jcapellman@hotmail.com>
2018-02-27 21:40:17 -0600

    Added: ExtensionMethod to ComputeSHA1

--- src/jcBENCH.lib/Benchmarks/HashingBenchmark.cs
+++ src/jcBENCH.lib/Benchmarks/HashingBenchmark.cs
@@ -1,30 +1,13 @@
 ﻿using System;
-using System.Security.Cryptography;
-using System.Text;
+
+using jcBENCH.lib.Common;
 
 namespace jcBENCH.lib.Benchmarks
 {
     public class HashingBenchmark : BaseBenchmark
     {
-        private static string ComputeSHA(string stringToHash)
-        {
-            using (var sha = new SHA1Managed())
-            {
-                var hashBytes = sha.ComputeHash(Encoding.ASCII.GetBytes(stringToHash));
-
-                var sb = new StringBuilder();
-
-                foreach (var b in hashBytes)
-                {
-                    sb.Append(b.ToString("X2"));
-                }
-
-                return sb.ToString();
-            }
-        }
-
         protected override string Name => "Hashing";
 
-        protected override string RunBenchmark() => ComputeSHA(DateTime.Now.Ticks.ToString());
+        protected override string RunBenchmark() => DateTime.Now.Ticks.ToString().ComputeSHA1();
     }
 }
---
+++ src/jcBENCH.lib/Common/ExtensionMethods.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace jcBENCH.lib.Common
+{
+    public static class ExtensionMethods
+    {
+        private static char GetHexValue(int value)
+        {
+            if (value < 0 || value > 15)
+            {
+                throw new ArgumentOutOfRangeException("value", "value must be between 0 and 15.");
+            }
+
+            if (value < 10)
+            {
+                return (char)(value + '0');
+            }
+
+            return (char)(value - 10 + 'a');
+        }
+
+        public static string ComputeSHA1(this String str)
+        {
+            using (var sha = new SHA1Managed())
+            {
+                var hashBytes = sha.ComputeHash(Encoding.ASCII.GetBytes(str));
+
+                var stringArray = hashBytes.Length * 2;
+                var charArray = new char[stringArray];
+                var index = 0;
+
+                for (var x = 0; x < stringArray; x += 2)
+                {
+                    var byteValue = hashBytes[index++];
+
+                    charArray[x] = GetHexValue(byteValue / 16);
+                    charArray[x + 1] = GetHexValue(byteValue % 16);
+                }
+
+                return new string(charArray);
+            }
+        }
+    }
+}

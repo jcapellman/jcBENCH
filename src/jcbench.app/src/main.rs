3d1959f6955242c46514e42e0e905f11d8743e9b
jcapellman <jcapellman@hotmail.com>
2024-05-31 22:19:16 -0500

    Added: Output closely matching the C# implementation

--- src/jcbench.app/src/main.rs
+++ src/jcbench.app/src/main.rs
@@ -1,9 +1,9 @@
 use chrono;
 use md5;
-
+use sysinfo::System;
 
 fn bench_md5() -> i32 {
-    let seconds_to_run:i64 = 20;
+    let seconds_to_run:i64 = 2;
 
     let start_time = chrono::offset::Local::now();
 
@@ -11,7 +11,7 @@ fn bench_md5() -> i32 {
 
     while (chrono::offset::Local::now() - start_time).num_seconds() < seconds_to_run
     {
-        let result:md5::Digest = md5::compute(b"abcdefghijklmnopqrstuvwxyz");
+        let _result:md5::Digest = md5::compute(chrono::offset::Local::now().to_string());
         
         if (chrono::offset::Local::now() - start_time).num_seconds() > seconds_to_run
         {
@@ -24,8 +24,60 @@ fn bench_md5() -> i32 {
     return number_iterations;
 }
 
+fn write_centered(str: String) {
+    println!("{}", str);
+}
+
+fn submit_result(benchmark: i32) -> bool {
+    // serialize and submit to server
+    return true;
+}
+
 fn main() {
     let result:i32 = bench_md5();
 
+    // Console.BackgroundColor = ConsoleColor.Black;
+    
+    // Console.Clear();
+
+    write_centered("jcBENCH 2024.5.0 (RUST Edition)".to_string());
+    write_centered("(C) 2012-2024 Jarred Capellman".to_string());
+    write_centered("Source code is available on https://github.com/jcapellman/jcBENCH".to_string());
+
+    //Console.BackgroundColor = ConsoleColor.Black;
+
+    let mut sys = System::new_all();
+    
+    sys.refresh_all();
+
+    println!("Operating System:             {:?}", System::name());
+    
+    println!("---------------");
+    println!("CPU Information");
+    println!("---------------");
+    println!("Manufacturer: {}", sys.global_cpu_info().brand());
+    println!("Model: {}", sys.global_cpu_info().name());
+    println!("Count: {}x{}", sys.cpus().len(), sys.global_cpu_info().frequency());
+    println!("Architecture: {}", sys.global_cpu_info().brand());
+    println!("---------------");
+
     println!("Hashing Benchmark Score: {result}");
+
+    println!("Do you want to submit your result (y/n)?");
+
+    let mut key = String::new();
+
+    let _ = std::io::stdin().read_line(&mut key);
+
+    if key.trim_end() != "y" {
+        return;
+    }
+
+    let submission_result = submit_result(result);
+
+    if submission_result {
+        println!("Submission was successful");
+    } else {
+        println!("Submission failed");
+    }
 }

4339debefa139d6dac5933af19b7fa71c20c73b0
Jarred Capellman <jcapellman@hotmail.com>
2024-06-05 19:00:48 -0500

    Added: Benchmark Name to the UI

--- src/jcBENCH.MVC/Objects/TopResultsListingItem.cs
+++ src/jcBENCH.MVC/Objects/TopResultsListingItem.cs
@@ -2,9 +2,11 @@
 {
     public class TopResultsListingItem
     {
-        public string CPUManufacturer { get; set; }
+        public required string CPUManufacturer { get; set; }
 
-        public string CPUModelName { get; set; }
+        public required string BenchmarkName { get; set; }
+
+        public required string CPUModelName { get; set; }
 
         public int BenchmarkResult { get; set; }
     }

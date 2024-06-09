37ff710cff1cc23e7a726b262710afa0f4b3e977
Jarred Capellman <jcapellman@hotmail.com>
2024-06-09 15:53:44 -0500

    Added: Database Migrations for the new columns Fix: Break out to only include the newest version of the results

---
+++ src/jcBENCH.MVC/Migrations/20240609203156_AdditionalColumns.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace jcBENCH.MVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdditionalColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BenchmarkAPIVersion",
+                table: "BenchmarkResults",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BenchmarkThreadingModel",
+                table: "BenchmarkResults",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BenchmarkAPIVersion",
+                table: "BenchmarkResults");
+
+            migrationBuilder.DropColumn(
+                name: "BenchmarkThreadingModel",
+                table: "BenchmarkResults");
+        }
+    }
+}

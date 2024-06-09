10b20322db7b809fe4f7347a3dec365d69ca663e
Jarred Capellman <jcapellman@hotmail.com>
2024-06-09 10:17:14 -0500

    Change: Cleaned up the Home Controller

--- src/jcBENCH.MVC/Controllers/HomeController.cs
+++ src/jcBENCH.MVC/Controllers/HomeController.cs
@@ -1,29 +1,17 @@
 using jcBENCH.MVC.DAL;
+
 using Microsoft.AspNetCore.Mvc;
 
 namespace jcBENCH.MVC.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController(MainDbContext dbContext) : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        private readonly MainDbContext _dbContext;
-
-        public HomeController(ILogger<HomeController> logger, MainDbContext dbContext)
-        {
-            _dbContext = dbContext;
-            _logger = logger;
-        }
+        private readonly MainDbContext _dbContext = dbContext;
 
         public ActionResult Downloads() => View();
 
         public ActionResult About() => View();
 
-        public ActionResult Index()
-        {
-            var topResults = _dbContext.BenchmarkResults.OrderByDescending(a => a.BenchmarkResult).ToList();
-
-            return View(topResults);
-        }
+        public ActionResult Index() => View(_dbContext.BenchmarkResults.OrderByDescending(a => a.BenchmarkResult).ToList());
     }
 }

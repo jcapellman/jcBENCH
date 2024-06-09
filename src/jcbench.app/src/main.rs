ae94c35b81472aa9a546762b288f2e5fbdbf951f
Jarred Capellman <jcapellman@hotmail.com>
2024-06-09 11:45:40 -0500

    Change: Cleaned up code

--- src/jcbench.app/src/main.rs
+++ src/jcbench.app/src/main.rs
@@ -1,13 +1,15 @@
+use crate::benchmark_submission::submit_result;
+use crate::benchmark_submission::BenchmarkRequest;
 use std::env;
 
 use chrono;
 use sysinfo::System;
-use serde::Serialize;
 
 mod benchmark;
 mod benchmark_md5;
 mod benchmark_sha1;
 mod cpuinfo;
+mod benchmark_submission;
 
 use crate::benchmark::Benchmark;
 
@@ -19,18 +21,6 @@ enum Benchmarks {
 
 pub struct BenchmarkSettings {
 	multi_threaded: bool
-}
-
-#[derive(Serialize, Debug)]
-pub struct BenchmarkRequest {
-    os_name: String,
-    benchmark_name: String,
-    benchmark_threading_model: String,
-    benchmark_api_version: u32,
-    cpu_name: String,
-    cpu_architecture: String,
-    cpu_cores: usize,
-    score: u32
 }
 
 fn run_benchmark<T: Benchmark>(selected_benchmark: T) -> u32 {
@@ -53,22 +43,6 @@ fn run_benchmark<T: Benchmark>(selected_benchmark: T) -> u32 {
     }
 
     return number_iterations;
-}
-
-fn write_centered(str: String) {
-    println!("{:=^50}", str);
-}
-
-fn submit_result(benchmark_result: BenchmarkRequest) -> bool {
-    println!("Submitting score to the server...");
-    
-    let client = reqwest::blocking::Client::new();
-
-    let response = client.post("https://www.jcbench.com/api/ResultSubmission")
-        .json(&benchmark_result)
-        .send().unwrap();
-    
-    return response.status() == 200;
 }
 
 fn parse_args(args: Vec<String>) -> (Box<dyn Benchmark>, Benchmarks) {
@@ -99,7 +73,7 @@ fn parse_args(args: Vec<String>) -> (Box<dyn Benchmark>, Benchmarks) {
     return (selected_benchmark, benchmark_selection);
 }
 
-fn retrieve_sysinfo(selected_benchmark_name: Benchmarks, benchmark_result: u32) -> BenchmarkRequest {
+fn retrieve_sysinfo(selected_benchmark_name: &Benchmarks, benchmark_result: u32) -> BenchmarkRequest {
     let mut sys = System::new_all();
     
     sys.refresh_all();
@@ -126,31 +100,35 @@ fn retrieve_sysinfo(selected_benchmark_name: Benchmarks, benchmark_result: u32) 
 }
 
 fn main() {
-    write_centered("jcBENCH 2024.5.0 (RUST Edition)".to_string());
-    write_centered("(C) 2012-2024 Jarred Capellman".to_string());
-    write_centered("Source code is available on https://github.com/jcapellman/jcBENCH".to_string());
+    println!("jcBENCH 2024.6.0 (RUST Edition)");
+    println!("(C) 2012-2024 Jarred Capellman");
+    println!("Source code is available on https://github.com/jcapellman/jcBENCH");
     
     let args: Vec<String> = env::args().collect();
 
     let (selected_benchmark, selected_benchmark_name) = parse_args(args);
 
     let result:u32 = run_benchmark(selected_benchmark);
-   
-    println!("Benchmark Score: {result}");
 
-    let benchmark_result = retrieve_sysinfo(selected_benchmark_name, result);
+    let benchmark_result = retrieve_sysinfo(&selected_benchmark_name, result);
 
-    println!("Operating System: {}", benchmark_result.os_name);
-    
+    println!("----------------");
+    println!("Benchmark Result");
+    println!("----------------");
+    println!("Type: {}", &selected_benchmark_name);
+    println!("Score: {result}");
+    println!("----------------\n");
+
     println!("---------------");
-    println!("CPU Information");
+    println!("Machine Information");
     println!("---------------");
     println!("Model: {}", benchmark_result.cpu_name);
     println!("Count: {}", benchmark_result.cpu_cores);
     println!("Architecture: {}", benchmark_result.cpu_architecture);
+    println!("Operating System: {}", benchmark_result.os_name);
     println!("---------------");
     
-    println!("Do you want to submit your result (y/n)?");
+    println!("Do you want to submit your result with the above information (y/n)?");
 
     let mut key = String::new();
 

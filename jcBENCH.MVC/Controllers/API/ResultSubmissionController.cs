c7924e2e44469279017923b297817fa7b7638634
Jarred Capellman <jcapellman@hotmail.com>
2024-06-19 08:46:24 -0700

    Change: Moved the ResultSubmissionController to the API folder

--- jcBENCH.MVC/Controllers/API/ResultSubmissionController.cs
+++ jcBENCH.MVC/Controllers/API/ResultSubmissionController.cs
@@ -1,13 +1,12 @@
-﻿using jcBENCH.MVC.DAL;
+﻿using jcBENCH.MVC.Controllers.API.Base;
+using jcBENCH.MVC.DAL;
 using jcBENCH.MVC.Objects;
 using Microsoft.AspNetCore.Mvc;
 using Results = jcBENCH.MVC.DAL.Objects.Results;
 
-namespace jcBENCH.MVC.Controllers
+namespace jcBENCH.MVC.Controllers.API
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ResultSubmissionController(MainDbContext dbContext) : ControllerBase
+    public class ResultSubmissionController(MainDbContext dbContext) : BaseApiController
     {
         [HttpPost]
         public async Task<bool> SubmitAsync([FromBody] ResultSubmissionItem submissionItem)

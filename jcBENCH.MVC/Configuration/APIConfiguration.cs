72567180c9a9cd2ce8456e16768681f79290de48
Jarred Capellman <jcapellman@hotmail.com>
2024-06-19 08:45:06 -0700

    Added: API Configuration class

---
+++ jcBENCH.MVC/Configuration/APIConfiguration.cs
@@ -0,0 +1,15 @@
+﻿namespace jcBENCH.MVC.Configuration
+{
+    public class ApiConfiguration
+    {
+        public required string JWTSecret { get; set; }
+
+        public required string JWTAudience { get; set; }
+
+        public required string JWTIssuer { get; set; }
+
+        public required string JWTSubject { get; set; }
+
+        public required string JWTHashToken { get; set; }
+    }
+}

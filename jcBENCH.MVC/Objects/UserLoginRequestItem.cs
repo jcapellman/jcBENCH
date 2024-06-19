8fe740556e673a3ee45488d8933e026aabfbe843
Jarred Capellman <jcapellman@hotmail.com>
2024-06-19 08:46:40 -0700

    Added: UserLoginRequestItem object

---
+++ jcBENCH.MVC/Objects/UserLoginRequestItem.cs
@@ -0,0 +1,9 @@
+﻿namespace jcBENCH.MVC.Objects
+{
+    public class UserLoginRequestItem
+    {
+        public required string UserName { get; set; }
+
+        public required string Password { get; set; }
+    }
+}

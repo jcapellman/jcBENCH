fa11bd6cfbe9597d0e8052ab8cb6f78a67d23e28
Jarred Capellman <jcapellman@hotmail.com>
2024-06-19 08:44:54 -0700

    Added: SHA256 Extension Method

---
+++ jcBENCH.MVC/Common/Extensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace jcBENCH.MVC.Common
+{
+    public static class Extensions
+    {
+        public static string ToSha256(this string str) => BitConverter.ToString(SHA256.HashData(Encoding.ASCII.GetBytes(str))).Replace("-", "").ToUpper();
+    }
+}

3aa4722d0be34c92ec3331413c937bddece0d934
Jarred Capellman <jcapellman@hotmail.com>
2024-06-19 20:38:26 -0700

    Added: Command Line parsing

--- jcbench.app/crates/benchmark_settings/src/lib.rs
+++ jcbench.app/crates/benchmark_settings/src/lib.rs
@@ -16,18 +16,36 @@ pub fn parse_args(args: Vec<String>) -> BenchmarkSettings {
         seconds_to_run: 20,
         selected_benchmark: AvailableBenchmarks::MD5
     };
-    
-    if args.len() > 1 {
-        for arg in args {
-            match arg.as_str() {
-                "sha1" => settings.selected_benchmark = AvailableBenchmarks::SHA1,
-                "md5" => settings.selected_benchmark = AvailableBenchmarks::MD5,
-                "multithreaded" => settings.multi_threaded = true,
-                _ => println!("Unexpected argument"),
+
+    if args.len() >= 3 {
+        let mut x = 1;
+
+        while x < args.len() {
+            match args[x].as_str() {
+                "benchmark" => {
+                    match args[x+1].as_str() {
+                        "sha1" => settings.selected_benchmark = AvailableBenchmarks::SHA1,
+                        "md5" => settings.selected_benchmark = AvailableBenchmarks::MD5,
+                        _ => eprintln!("Invalid benchmark argument...using default"),
+                    }
+                },
+                "duration" => {
+                    settings.seconds_to_run = i64::from_str_radix(args[x+1].as_str(), 10).unwrap();
+                },
+                "threading" => {
+                    match args[x+1].as_str() {
+                        "single" => settings.multi_threaded = false,
+                        "multi" => settings.multi_threaded = true,
+                        _ => println!("Invalid threading argument..using default"),
+                    }
+                },
+                _ => println!("Unexpected argument ({})", args[x].as_str()),
             }
+
+            x += 2;
         }
     } else {
-        println!("No arguments passed in...running defaults...");
+        println!("No or invalid arguments passed in...running defaults...");
     }
     
     return settings;
